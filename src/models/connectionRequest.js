90fc77799971de01a3b80364095c597982c3107b
Ronik <ronik.kalanouria@gmail.com>
2024-12-19 14:58:34 +0530

    connectionRequestSchema added

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const connectionRequestSchema = mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignore", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("ConnectionRequest", connectionRequestSchema);
